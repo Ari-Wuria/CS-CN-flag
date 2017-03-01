cecb1036233a4da9f3b4bc26d38b3983b3a9b3a2
Egor Aralov <aei.omsk@gmail.com>
2017-03-01 13:55:14 +0600

    Add to source control

---
+++ ShroomPack/ShroomPack.cs
@@ -0,0 +1,28 @@
+﻿using ICities;
+
+namespace ShroomPack
+{
+    public class ShroomPack : IUserMod
+    {
+        public string Name
+        {
+            get { return "Shroom flag pack"; }
+        }
+
+        public string Description
+        {
+            get { return "Shroom flag pack"; }
+        }
+
+        public string[][] CustomFlags()
+        {
+            return new[]
+            {
+                new[] {"hippy", "Hippy"},
+                new[] {"colors", "Colors #1"},
+                new[] {"colors2", "Colors #2"},
+                new[] {"business", "Business"}
+            };
+        }
+    }
+}
